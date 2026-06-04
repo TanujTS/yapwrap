6b485e12cf178b46fe6d2ce42012837eb4eb7483
Tanuj <tanujts20@gmail.com>
2026-06-05 03:23:51 +0530

    chore: clean test scripts

--- apps/api/test-logger2.ts
+++
@@ -1,4 +0,0 @@
-import { env } from "./src/config/env";
-import { getLoggerConfig } from "./src/logger/config";
-console.log("NODE_ENV:", env.NODE_ENV);
-console.log("isDevelopment:", getLoggerConfig().isDevelopment);
--- apps/api/test-pino-stream.ts
+++
@@ -1,6 +0,0 @@
-import pino from "pino";
-import pretty from "pino-pretty";
-
-const stream = pretty();
-const logger = pino({ level: "info" }, stream);
-logger.info("testing pretty stream directly");
